416044ef3f77ec24c31862c28969369551d47234
Lee Haewon <hawon2205@ewhain.net>
2024-01-28 21:10:04 +0900

    add: @EnableJpaAuditing 추가 (#1)

--- src/main/java/com/gdsc/hearo/HearoApplication.java
+++ src/main/java/com/gdsc/hearo/HearoApplication.java
@@ -2,12 +2,15 @@ package com.gdsc.hearo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class HearoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HearoApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(HearoApplication.class, args);
+    }
 
 }
