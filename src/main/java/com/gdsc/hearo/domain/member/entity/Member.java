3f19e33ec42783cead0164fd84a9a6e328e09ba0
Lee Haewon <hawon2205@ewhain.net>
2024-02-01 16:52:03 +0900

    refactor: user 엔티티명 변경 (#3)

--- src/main/java/com/gdsc/hearo/domain/member/entity/Member.java
+++ src/main/java/com/gdsc/hearo/domain/member/entity/Member.java
@@ -1,4 +1,4 @@
-package com.gdsc.hearo.domain.user.entity;
+package com.gdsc.hearo.domain.member.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
@@ -8,20 +8,20 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.sql.Timestamp;
 
 @Entity
-@Table(name = "user")
+@Table(name = "member")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 @Builder
-public class User {
+public class Member {
     public enum LoginType {
         DEFAULT, GOOGLE
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id", nullable = false)
+    @Column(name = "member_id", nullable = false)
     private Long userId; // 기본키
 
     @Column(name = "login_id")
@@ -46,5 +46,5 @@ public class User {
 
     @OneToOne
     @JoinColumn(name = "setting_id")
-    private UserSetting userSetting;
+    private MemberSetting memberSetting;
 }
