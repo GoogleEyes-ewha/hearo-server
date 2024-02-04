663e3eeeb8977fbbf64e7220e1ebf7feeceda65b
yslim21 <yslim317@gmail.com>
2024-02-05 01:46:02 +0900

    feat: 상품 찜기능 구현1

---
+++ src/main/java/com/gdsc/hearo/domain/item/controller/WishController.java
@@ -0,0 +1,44 @@
+package com.gdsc.hearo.domain.item.controller;
+
+import com.gdsc.hearo.domain.item.dto.WishRequestDto;
+import com.gdsc.hearo.domain.item.dto.WishResponseDto;
+import com.gdsc.hearo.domain.item.service.WishService;
+import com.gdsc.hearo.global.common.BaseResponse;
+import com.gdsc.hearo.global.common.BaseResponseStatus;
+import com.gdsc.hearo.global.security.CustomUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/item/wish")
+public class WishController {
+
+    private final WishService wishService;
+
+    @Autowired
+    public WishController(WishService wishService){
+        this.wishService = wishService;
+    }
+
+    @PostMapping
+    public ResponseEntity<BaseResponse<WishResponseDto>> addToWishList(@AuthenticationPrincipal CustomUserDetails userDetails,
+
+                                                                       @RequestBody WishRequestDto wishRequestDto){
+        BaseResponse<WishResponseDto> response;
+
+        try{
+            Long userId = userDetails.getMember().getMemberId();
+            WishResponseDto result = wishService.addToWishList(userId, wishRequestDto.getItemId());
+
+            response = new BaseResponse<>(BaseResponseStatus.SUCCESS, result);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            response = new BaseResponse<>(BaseResponseStatus.INTERNAL_SERVER_ERROR, null);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+    }
+
+}
