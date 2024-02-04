a465b0a7cb67ae531f49473c8385363be0bb88c9
yslim21 <yslim317@gmail.com>
2024-02-04 11:26:04 +0900

    feat: 상품 STT 검색 구현2

--- src/main/java/com/gdsc/hearo/domain/item/repository/ItemRepository.java
+++ src/main/java/com/gdsc/hearo/domain/item/repository/ItemRepository.java
@@ -2,8 +2,7 @@ package com.gdsc.hearo.domain.item.repository;
 
 import com.gdsc.hearo.domain.item.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 import java.util.List;
 
