314666f08a0a0c93a5a8294914f66c11dd335ac0
Lee Haewon <hawon2205@ewhain.net>
2024-02-07 17:51:30 +0900

    feat: Security config 설정 추가 (#3)

--- src/main/java/com/gdsc/hearo/global/config/SecurityConfig.java
+++ src/main/java/com/gdsc/hearo/global/config/SecurityConfig.java
@@ -1,19 +1,22 @@
 package com.gdsc.hearo.global.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gdsc.hearo.global.security.CustomUserDetailsService;
 import com.gdsc.hearo.global.security.JwtAuthFilter;
+import com.gdsc.hearo.global.security.JwtExceptionFilter;
 import com.gdsc.hearo.global.security.JwtUtil;
-import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.time.Duration;
@@ -24,18 +27,24 @@ import java.time.Duration;
 public class SecurityConfig {
 
     private final JwtUtil jwtUtil;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
+
         corsConfig.addAllowedOrigin("http://localhost:3000");
+        corsConfig.addAllowedOrigin("http://localhost:8080");
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(true);
         corsConfig.setMaxAge(Duration.ofSeconds(3600));
+        corsConfig.addExposedHeader("Authorization");
+        corsConfig.addExposedHeader("Authorization-Refresh");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
+        source.registerCorsConfiguration("/**", corsConfig); // 설정한 Configuration 적용
 
         return source;
     }
@@ -50,11 +59,16 @@ public class SecurityConfig {
                 ))
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtAuthFilter(customUserDetailsService, jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionFilter(objectMapper), JwtAuthFilter.class)
                 .authorizeHttpRequests(httpRequests -> httpRequests
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                        .requestMatchers("/user/signup").permitAll()
+                        .requestMatchers("/user/login").permitAll()
                         .anyRequest().permitAll()
-                );
+                )
+
+        ;
         return http.build();
     }
 
